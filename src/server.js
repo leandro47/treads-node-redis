6501d060984914b95fb46f16d9e434d252e0423b
leandro47 <leandrosilva47@live.com>
2020-08-25 23:22:50 -0300

    created post

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
 import express from  'express';
 
+import UserController from '.app/controllers/UserController';
+
 const app = express();
 
 app.use(express.json());
 
+app.post('/users', UserController.store);
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on the ${process.env.PORT}`)
+    console.log(`Server running in the port ${process.env.PORT}`)
 });
+
