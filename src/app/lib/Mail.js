14c3341fcad9cfed8e349b24e2df849abdc6aca1
leandro47 <leandrosilva47@live.com>
2020-08-27 21:10:15 -0300

    corrigindo bugs

--- src/app/lib/Mail.js
+++ src/app/lib/Mail.js
@@ -2,4 +2,11 @@ import nodemailer from 'nodemailer';
 import mailConfig from '../../config/mail';
 
 
-export default nodemailer.createTransport({mailConfig})
+export default nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ef908eb213ff57",
+      pass: "447c4fa38617e3"
+    }
+  });
